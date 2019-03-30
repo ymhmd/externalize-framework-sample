047d361ca46083638632e705b8b4257140353be3
Yasin Mhmd <ymhmd082@gmail.com>
2019-03-30 05:02:06 +0200

    Add files via upload

---
+++ src/main/java/com/externalize/sample.java
@@ -0,0 +1,21 @@
+package com.externalize;
+
+public class sample {
+
+    public void sampleMethod1 () {
+        System.out.println("SAMPLE METHOD 1.");
+    }
+
+    public void sampleMethod2 () {
+        System.out.println("SAMPLE METHOD 2.");
+    }
+
+    public void sampleMethod3 () {
+        System.out.println("SAMPLE METHOD 3.");
+    }
+
+    public void sampleMethod4 () {
+        System.out.println("SAMPLE METHOD 4.");
+    }
+
+}
